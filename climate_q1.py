ff7e9e4a7ac16463be8378464fb02e2120df397a
jas w <jas-wicks@live.com>
2023-09-16 17:01:17 +1000

    Completed question 1

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,21 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+#Declare variables to store lists
 years = []
 co2 = []
 temp = []
+
+connection = sqlite3.connect(r"C:\\Users\\jas-w\Documents\\projects\\2810ICT\\Quiz\\Quiz_week_8\\climate.db")
+cursor = connection.cursor()
+
+cursor.execute("SELECT*FROM ClimateData")
+result = cursor.fetchall() 
+
+#Iterate through rows and append to lists
+for r in result:
+    years.append(r[0])
+    co2.append(r[1])
+    temp.append(r[2])
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
